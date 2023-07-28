03053ea513059ecd335de5b9ad50687d8055dc26
Erfan Sabouri <erfaansabouri@gmail.com>
2023-07-28 21:17:57 +0330

    Admin dashboard

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('group_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('national_code')->nullable();
+            $table->string('birthdate')->nullable();
+            $table->string('password')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ resources/views/admin/partials/scripts.blade.php
@@ -0,0 +1,4 @@
+<script>var hostUrl = "assets/";</script>
+<!--begin::Global Javascript Bundle(mوatory for all pages)-->
+<script src="{{ asset('global-assets/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('global-assets/js/scripts.bundle.js') }}"></script>
